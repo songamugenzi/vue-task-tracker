c2cb8cab96d5a99f8e2a16574a2338507bfad4e3
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2022-06-20 17:21:02 -0400

    Adding a task is functional and the form can be toggled to open or close

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker"/> 
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/> 
+    <div v-show="showAddTask" >
+      <AddTask @add-task="addTask" />
+    </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>  
@@ -8,19 +11,28 @@
 <script>
 import Header from './components/Header.vue'
 import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
     Tasks,
+    AddTask
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
     deleteTask(id) {
       if (confirm('Are you sure?')) {
         this.tasks = this.tasks.filter((task) => task.id !== id)
